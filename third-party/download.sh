199dc5a6d220edc376d1e3721bcfb65552e5a1d5
ndylmer <dylmer123@gmail.com>
2026-02-06 11:30:09 +0100

    changed download.sh to use old monpoly commit

--- third-party/download.sh
+++ third-party/download.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+cd third-party
 git clone https://bitbucket.org/jshs/monpoly.git
+cd monpoly
+git checkout d0dc92d40ecab5fe088470125a0681a010e2a12b
+cd ..
 git clone https://github.com/leonardolima/dejavu-online.git
+git clone https://git.ku.dk/kfx532/timelymon.git
+
